e424a93d3781ae642703ef09f0dde32e63564286
Luan <geremiadluan@gmail.com>
2026-01-26 19:56:06 -0300

    Fiz mais exercicios

---
+++ ex077.py
@@ -0,0 +1,5 @@
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON',
+            'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR',
+            'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+for c in palavras:
+    print(f'Na palavra {c} temos {} ')
