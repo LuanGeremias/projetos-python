f5763ca74bd524693298e4423622fbf8619bdf0b
Luan <geremiadluan@gmail.com>
2026-01-26 20:07:40 -0300

    Finalizei o ecercicios 77

--- ex077.py
+++ ex077.py
@@ -1,5 +1,8 @@
 palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON',
             'CURSO', 'GRATIS', 'ESTUDAR', 'PRATICAR',
             'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
-for c in palavras:
-    print(f'Na palavra {c} temos {} ')
+for p in palavras:
+    print(f'\nNa palavra {p} temos ', end=' ')
+    for letra in p:
+        if letra in 'AEIOU':
+            print(letra.lower(), end=' ')
