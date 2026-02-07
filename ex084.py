53d2eb0d949ee798fe10c176ecf01477dad720d4
Luan <geremiadluan@gmail.com>
2026-02-06 21:46:16 -0300

    Tentando melhorar o codigo

--- ex084.py
+++ ex084.py
@@ -1,21 +1,2 @@
-galera = list()
-dados = list()
-pesados = list()
-leves = list()
-cont = 0
+pessoas = list()
 while True:
-    galera.append(str(input('Nome: ')))
-    galera.append(float(input('Peso: ')))
-    dados.append(galera[:])
-    galera.clear()
-    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    if resp == 'N':
-        break
-print(f'Ao total foram {len(dados)} cadrastrados.')
-for p in dados:
-    if cont == 0:
-        pesados.append(p)
-        leves.append(p)
-    elif p[1][] >
-    cont += 1
-print(pesados)
