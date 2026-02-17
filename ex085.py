95facd00a4405d5ba2d8ecf99d32f1d55929147c
Luan <geremiadluan@gmail.com>
2026-02-17 20:50:08 -0300

    update ex085

--- ex085.py
+++ ex085.py
@@ -1,15 +1,11 @@
-numeros = list()
-pares = list()
-impares = list()
+numeros = [[], []]
 for c in range(1, 8):
     n1 = int(input('Digite um valor: '))
     if n1 % 2 == 0:
-        pares.append(n1)
+        numeros[0].append(n1)
     else:
-        impares.append(n1)
-pares.sort()
-impares.sort()
-numeros.append(pares[:])
-numeros.append(impares[:])
+        numeros[1].append(n1)
+numeros[0].sort()
+numeros[1].sort()
 print(f'Os valores pares digitados foram {numeros[0]}')
 print(f'Os valores impares digitados foram {numeros[1]}')
