b6d9276047de97d7a1d876ea4295020adcc3d7b0
Luan <geremiadluan@gmail.com>
2026-02-26 20:36:59 -0300

    Quase terminando o ex090

--- ex090.py
+++ ex090.py
@@ -1,9 +1,10 @@
-dic = {}
-nome = str(input('Nome: '))
-media = str(input(f'Média de {nome}: '))
-dic = ('Nome': nome, 'Média': media)
-if media >= 6:
-    dic['Situação'] = 'Aprovado'
-else
-    dic['Situação'] = 'Reprovado'
-print(f'{dic.values()} é igual a {dic["nome"]}')
+lista = list()
+dic = dict()
+dic['nome'] = str(input('Nome: ')).strip().upper()
+dic['média'] = float(input('Média: '))
+if dic['média'] >= 7:
+    dic['Situação': 'Aprovado']
+else:
+    dic['Situação': 'Reprovado']
+for k, v in dic.items():
+    print(f'{k} é igual a {v}')
