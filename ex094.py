8f1a51c950f6d6cd36663742fd540c091d092e92
Luan <geremiadluan@gmail.com>
2026-03-03 19:40:46 -0300

    melhorei o ex94

--- ex094.py
+++ ex094.py
@@ -1,25 +1,28 @@
 pessoa = {}
 lista = list()
-somaidade = cont = 0
+somaidade = 0
 while True:
-    cont += 1
     pessoa['nome'] = str(input('Nome: '))
-    pessoa['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
-    while pessoa['sexo'] not in 'MF':
-        pessoa['sexo'] = str(input('Erro! Responda apenas M ou F: ')).strip().upper()[0]
+    while True:
+        pessoa['sexo'] = str(input('Sexo [M/F] ')).strip().upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Responda apenas M ou F.')
     pessoa['idade'] = int(input('Idade: '))
     somaidade += pessoa['idade']
     lista.append(pessoa.copy())
-    resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
-    while resp not in 'SN':
-        resp = str(input('Erro! Responda apenas S ou N ')).strip().upper()[0]
+    while True:
+        resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+        if resp in 'NS':
+            break
+        print('ERRO! Responda apenas S ou N ')
     if resp == 'N':
         break
 
 print('-=' * 30)
 print(f' - O grupo tem {len(lista)} pessoas.')
-media = somaidade / cont
-print(f' - A média de idade é de {media:2} anos.')
+media = somaidade / len(lista)
+print(f' - A média de idade é de {media:.2f} anos.')
 print(' - As mulheres cadastradas foram: ', end='')
 for c in lista:
     if c['sexo'] == 'F':
@@ -28,6 +31,9 @@ print()
 print(' - Lista das pessoas que estão acima da média: ')
 for c in lista:
     if c['idade'] >= media:
-        print(f'   - {c}', end=' ')
+        print('     ')
+        for k, v in c.items():
+            print(f'{k} = {v}; ', end='')
         print()
+print('<< ENCERRADO >>')
 
